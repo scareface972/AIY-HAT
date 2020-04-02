ccd0a5e541828d6a154432be46769c624e0ed2f8
scareface972 <j.rolle972@gmail.com>
2020-04-02 11:38:10 -0400

    Update install-alsa-config.sh

--- install-alsa-config.sh
+++ install-alsa-config.sh
@@ -42,6 +42,6 @@ fi
 
 echo 'AIY-HAT' >> $audioconfig
 
-sudo cp scripts/asound.conf "$global_asoundrc"
-sudo cp scripts/.asoundrc "$asoundrc"
+sudo cp asound.conf "$global_asoundrc"
+sudo cp .asoundrc "$asoundrc"
 echo "Installed voiceHAT ALSA config at $global_asoundrc"
